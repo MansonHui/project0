30465b4be7ba76bc27bc4b48093b6dd965a81764
Ziner Fung <siner1987s@gmail.com>
2024-06-24 17:36:26 +0800

    login page join togrther

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,13 +1,12 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Login from './pages/login/Login';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        
-
       </header>
     </div>
   );
