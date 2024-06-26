1fc69cd342636c0b801adbb397cc0c16e9fce6de
Ziner Fung <siner1987s@gmail.com>
2024-06-26 14:42:11 +0800

    fetch api done

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,4 +1,4 @@
-import { Navigate, Route, Routes } from "react-router-dom";
+import { Route, Routes, useNavigate } from "react-router-dom";
 import "./App.css";
 import Login from "./pages/login/loginPage";
 import { LoginAuthGuard } from "./pages/login/loginAuthGuard";
@@ -7,52 +7,27 @@ import DrawPage from "./pages/drawNotice/DrawPage";
 import AIAttendances from "./pages/scan/AIAttendances";
 import { useAuth } from "./hooks/useAuth";
 import MessagePage from "./pages/message/MessagePage";
-import { useQuery } from "@tanstack/react-query";
-import MenuHeaderBar from "./components/header/Header";
-
-import NoticePage from "./pages/notice/NoticePage";
+import { useEffect } from "react";
 
 function App() {
   const { authToken } = useAuth();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    console.log("App")
+    if (authToken) navigate("/HomePage");
+    else navigate("/");
+  }, [authToken]);
   return (
-    <div>
-      
-      <Routes>
-      
+    <Routes>
       <Route path="/" element={<Login />} />
-<<<<<<< HEAD
-      {/* <Route element={<LoginAuthGuard authToken={authToken} />}> */}
+      <Route element={<LoginAuthGuard authToken={authToken} />}>
         <Route path="/HomePage" element={<HomePage />} />
         <Route path="/Message" element={<MessagePage />} />
         <Route path="/Drawing" element={<DrawPage />} />
         <Route path="/AI" element={<AIAttendances />} />
-=======
-      {/* <Route element={<LoginAuthGuard authToken={thToken} />}> */}
-      
-      
-      <Route
-          path="/*"
-          element={
-            <>
-              <MenuHeaderBar />
-              <Routes>
-                <Route path="/HomePage" element={<HomePage />} />
-                <Route path="/Message" element={<MessagePage />} />
-                <Route path="/Notices" element={<NoticePage />} />
-                <Route path="/Drawing" element={<DrawPage />} />
-                <Route path="/AI" element={<AIAttendances />} />
-              </Routes>
-            </>
-          }
-        />
-
-
->>>>>>> a10c5287c3f31f784b02f3963d6267c883b44f37
-      {/* </Route> */}
+      </Route>
     </Routes>
-
-    </div>
-    
   );
 }
 
--- frontend/src/pages/login/loginAuthGuard.tsx
+++ frontend/src/pages/login/loginAuthGuard.tsx
@@ -1,6 +1,7 @@
 import { Navigate, Outlet } from "react-router-dom";
 
 export function LoginAuthGuard(props: { authToken: string | null}) {
+  console.log("authGurad")
   if (props.authToken) {
     return <Outlet />;
   } else {
