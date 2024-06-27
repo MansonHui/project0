aee2eda13cbb805957c1a0b575cac852e5910cfd
Manson Hui <huichuenman@gmail.com>
2024-06-28 01:23:41 +0800

    testing

--- backend/server.ts
+++ backend/server.ts
@@ -28,7 +28,7 @@ app.use("/Message", attendanceAndNoticeRouter);
 
 //***************************************** */ testing
 app.post("/", function (req: Request, res: Response) {});
-console.log("GG3");
+console.log("1233456");
 
 app.listen(PORT, () => {
   console.log(`listening to http://localhost:${PORT}/`);
