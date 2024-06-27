c88fb31936bc7c12b8bfbeafd5e884a3057f10ec
Manson Hui <huichuenman@gmail.com>
2024-06-27 19:08:53 +0800

    bug fix

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -10,8 +10,8 @@ import AIAttendances from "./pages/scan/AIAttendances";
 import MessagePage from "./pages/message/MessagePage";
 import NoticePage from "./pages/notice/NoticePage";
 import AttendancePage from "./pages/attendance/AttendancePage";
-import RegisterPage from "./pages/register/RegisterPage";
-import ParentPage from "./pages/parent/ParentPage";
+// import RegisterPage from "./pages/register/RegisterPage";
+// import ParentPage from "./pages/parent/ParentPage";
 
 function App() {
   const { authToken } = useAuth();
@@ -26,15 +26,15 @@ function App() {
       <Route path="/" element={<Login />} />
       <Route element={<LoginAuthGuard authToken={authToken} />}>
         {/* <Route path="/HomePage" element={<HomePage />} /> */}
-        <Route path="/HomePage" element={<ParentPage />} />  
+        {/* <Route path="/HomePage" element={<ParentPage />} /> */}
         <Route path="/Message" element={<MessagePage />}>
           <Route path="Notices" element={<NoticePage />} />
           <Route path="Attendance" element={<AttendancePage />} />
         </Route>
         <Route path="/Drawing" element={<DrawPage />} />
         <Route path="/AI" element={<AIAttendances />} />
-        <Route path="/Register" element={<RegisterPage />} />
-    </Route>
+        {/* <Route path="/Register" element={<RegisterPage />} /> */}
+      </Route>
     </Routes>
   );
 }
