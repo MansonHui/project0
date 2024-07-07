ad29fdf8e86406663858346260e1ef896759eb45
Ziner Fung <siner1987s@gmail.com>
2024-07-07 18:08:35 +0800

    edit note page css done

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -26,7 +26,7 @@ function App() {
   const navigate = useNavigate();
 
   useEffect(() => {
-    if (authToken) navigate("/HomePage");
+    if (authToken) navigate("/EditNotice");
     else navigate("/");
   }, [authToken]);
 
