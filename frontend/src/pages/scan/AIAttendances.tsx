8dc129998173752f1a1b68a5c80f21f6ca064dc6
Ziner Fung <siner1987s@gmail.com>
2024-07-08 17:28:08 +0800

    Login 100% done

--- frontend/src/pages/scan/AIAttendances.tsx
+++ frontend/src/pages/scan/AIAttendances.tsx
@@ -1,10 +1,5 @@
 import styles from "./AIAttendances.module.css";
 
 export default function AIAttendances() {
-  return (
-    <form id={styles.AIAttendancesPage}>
-      <div id={styles.AIAttendances}></div>
-      <div id={styles.doubleTickIcon}></div>
-    </form>
-  );
+  return <></>;
 }
