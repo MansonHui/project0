61b3b9fd3ac004fe05222e1a97063240a55393c9
Ziner Fung <siner1987s@gmail.com>
2024-07-12 15:37:19 +0800

    add try catch

--- backend/controllers/AuthController.ts
+++ backend/controllers/AuthController.ts
@@ -11,60 +11,76 @@ export default class AuthController {
   constructor(private authService: AuthService) {}
 
   parentRegiser = async (req: Request, res: Response) => {
-    let existEmail = await this.authService.checkDuplicateEmail(req.body.email);
+    try {
+      let existEmail = await this.authService.checkDuplicateEmail(
+        req.body.email
+      );
 
-    if (existEmail) {
-      res.status(200).json({ msg: `${req.body.email} already exists` });
-      return;
+      if (existEmail) {
+        res.status(200).json({ msg: `${req.body.email} already exists` });
+        return;
+      }
+
+      let newParentdetail = await this.authService.createNewParent(
+        await getUserName(req.body.email),
+        req.body.password,
+        req.body.email
+      );
+
+      res.status(200).json({
+        msg: `${newParentdetail.email} established`,
+      });
+    } catch (e) {
+      console.error(e);
+      res.status(400).json({
+        msg: e,
+      });
     }
-
-    let newParentdetail = await this.authService.createNewParent(
-      await getUserName(req.body.email),
-      req.body.password,
-      req.body.email
-    );
-
-    res.status(200).json({
-      msg: `${newParentdetail.email} established`,
-    });
   };
 
   login = async (req: Request, res: Response) => {
-    let email = req.body.admin_email ? req.body.admin_email : req.body.email;
-    let password = req.body.password;
+    try {
+      let email = req.body.admin_email ? req.body.admin_email : req.body.email;
+      let password = req.body.password;
 
-    console.log(req.body);
+      console.log(req.body);
 
-    let loginUserData = await this.authService.login(email, password);
+      let loginUserData = await this.authService.login(email, password);
 
-    console.log("loginUserData", loginUserData);
+      console.log("loginUserData", loginUserData);
 
-    if (!loginUserData) {
-      res.status(400).json({ msg: "wrong password/user" });
-    } else {
-      const payload = {
-        userId: loginUserData.id,
-        userRole: Object.keys(loginUserData)[0],
-        email: loginUserData.email
-          ? loginUserData.email
-          : loginUserData.admin_email,
-        userName: loginUserData.username
-          ? loginUserData.username
-          : loginUserData.admin_name,
-        school_id: loginUserData.school_id ? loginUserData.school_id : null,
-      };
+      if (!loginUserData) {
+        res.status(400).json({ msg: "wrong password/user" });
+      } else {
+        const payload = {
+          userId: loginUserData.id,
+          userRole: Object.keys(loginUserData)[0],
+          email: loginUserData.email
+            ? loginUserData.email
+            : loginUserData.admin_email,
+          userName: loginUserData.username
+            ? loginUserData.username
+            : loginUserData.admin_name,
+          school_id: loginUserData.school_id ? loginUserData.school_id : null,
+        };
 
-      console.log("payload", payload);
+        console.log("payload", payload);
 
-      const jwtToken = jwtSimple.encode(payload, process.env.JWT_SECRET!);
+        const jwtToken = jwtSimple.encode(payload, process.env.JWT_SECRET!);
 
-      res.status(200).json({
-        msg: ` ${Object.keys(loginUserData)[0]},${email} :login success`,
-        token: jwtToken,
-        userRole: Object.keys(loginUserData)[0],
-        userName: loginUserData.username
-          ? loginUserData.username
-          : loginUserData.admin_name,
+        res.status(200).json({
+          msg: ` ${Object.keys(loginUserData)[0]},${email} :login success`,
+          token: jwtToken,
+          userRole: Object.keys(loginUserData)[0],
+          userName: loginUserData.username
+            ? loginUserData.username
+            : loginUserData.admin_name,
+        });
+      }
+    } catch (e) {
+      console.error(e);
+      res.status(400).json({
+        msg: e,
       });
     }
   };
