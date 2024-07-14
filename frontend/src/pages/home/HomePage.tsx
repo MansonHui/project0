dae2442abcd34e99ff0eea830f031cb0d0c3c1e7
Ziner Fung <siner1987s@gmail.com>
2024-07-14 18:13:41 +0800

    homepage img size

--- frontend/src/pages/home/HomePage.tsx
+++ frontend/src/pages/home/HomePage.tsx
@@ -25,14 +25,12 @@ export default function HomePage() {
             <div className={styles.UserSchool_Info}></div>
           </div>
           <div className={styles.UserChild_Container}>
-            <div className={styles.UserChild_photo}>
-              {/* {entry.image} */}
+            <img
+              className={styles.UserChild_photo}
+              src={`http://localhost:8080/images/${entry.image}`}
+              alt="student profile pic"
+            ></img>
 
-              <img
-                src={`http://localhost:8080/images/${entry.image}`}
-                alt="student profile pic"
-              ></img>
-            </div>
             <div className={styles.UserChild_Info}>
               <div className={styles.UserChild_Information}>
                 Teacher: {entry.admin_name}
